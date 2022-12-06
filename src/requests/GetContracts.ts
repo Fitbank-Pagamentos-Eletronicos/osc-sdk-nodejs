7b1324cb2e7688c22135f24c79cb37785d5d7da8
Fit-Iuri <iuri.mendes@fitbank.com.br>
2022-12-06 15:37:52 -0300

    feat: requisição getcontracts

---
+++ src/requests/GetContracts.ts
@@ -0,0 +1,30 @@
+import fetch, { RequestInit, Headers } from 'node-fetch';
+import { Contract } from '../domains/Contract';
+import { Auth } from '../domains/Auth';
+import { OAuth } from './OAuth';
+
+export const GetContracts = async (
+  customerServiceNumber: string,
+  auth: Auth
+) => {
+  const myHeaders = new Headers();
+
+  const oAuth = JSON.parse(await OAuth(auth));
+  const access_token = await oAuth.access_token;
+
+  myHeaders.append('Content-Type', 'application/json');
+  myHeaders.append('Accept', 'application/json');
+  myHeaders.append('Authorization', `Bearer ${access_token}`);
+
+  const requestOptions: RequestInit = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  const response = await fetch(
+    `https://demo-api.easycredito.com.br/api/external/v2.1/contract/${customerServiceNumber}`,
+    requestOptions
+  );
+  return response.text();
+};
