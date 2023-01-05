287482d269acb332188d05ff11cf56fe3e06abdc
Fit-Iuri <iuri.mendes@fitbank.com.br>
2023-01-05 09:53:22 -0300

    test: add teste em jest da contract GET

---
+++ src/tests/requests/GetContracts.test.ts
@@ -0,0 +1,28 @@
+import { GetContracts } from '../../requests/GetContracts';
+import { Auth } from '../../domains/Auth';
+import { GetContract } from '../../domains/GetContract';
+import { Scopes } from '../../domains/enums';
+
+test('the data is equal to GetContracts', async () => {
+  const auth = new Auth();
+  auth.setClient_id('iuri.mendes--------bde4-84fb8eb9865e');
+  auth.setClient_secret(
+    '2e1995260d53d2b028a2a47553bd823042c589226e15d4994a466a97ce692271'
+  );
+  auth.setScopes(Scopes.api_external);
+
+  const getContract = new GetContract();
+  getContract.setCustomerServiceNumber('20221213170333387004500');
+  const customerServiceNumber = JSON.stringify(
+    getContract.getCustomerServiceNumber()
+  );
+
+  return setTimeout(() => {
+    GetContracts('20221213170333387004500', auth).then((data) => {
+      expect(JSON.parse(data)).toMatchObject({
+        customerServiceNumber: expect.any(String),
+        contracts: expect.any(Array)
+      });
+    });
+  }, 10000);
+});
